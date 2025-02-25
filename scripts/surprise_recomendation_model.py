b29d5cb14182c2c7eb7c0f51c100d3e464440897
Hugo-Vaz <hugo.vaz30@gmail.com>
2025-02-25 18:06:48 -0300

    MLFlow adotado e salvando os modelos e artefatos com testes com sucesso
    Pra rodar o treino do  modelo antes precisamos rodar mlflow server --host 127.0.0.1 --port 8080 no console, imagino que isso vai ficar no docker compose

--- scripts/surprise_recomendation_model.py
+++ scripts/surprise_recomendation_model.py
@@ -4,13 +4,40 @@ from sklearn.compose import ColumnTransformer
 from surprise import accuracy, Dataset, SVD,Reader, KNNBaseline
 from surprise.model_selection import train_test_split
 import create_masterdata as master_data
+import mlflow
+from mlflow.models import infer_signature
 
-def runModel (algo, trainset,testset):
-    algo.fit(trainset)
-    predictions = algo.test(testset)
-
+def runModel (algo, train_set,test_set,experiment_name,hyper_params, model_name, description, input_sample):
+    mlflow.set_experiment(experiment_name=experiment_name)
+    algo.fit(train_set)
+    predictions = algo.test(test_set)
     # Then compute RMSE
-    accuracy.rmse(predictions)
+    rmse = accuracy.rmse(predictions)
+
+    with mlflow.start_run():
+        # Log the hyperparameters
+        mlflow.log_params(hyper_params)
+
+        # Log the loss metric
+        mlflow.log_metric("rmse", rmse)
+
+        # Set a tag that we can use to remind ourselves what this run was for
+        mlflow.set_tag("Training Info", description)
+
+        # Infer the model signature
+        signature = infer_signature(train_set, algo.test(test_set))
+
+        # Log the model
+        model_info = mlflow.sklearn.log_model(
+            sk_model=algo,
+            artifact_path="news_prediction",
+            signature=signature,
+            input_example=input_sample,
+            registered_model_name=model_name,
+        )
+    
+
+   
 
 
 def scaleData(dataset):
@@ -31,8 +58,11 @@ def modelPredict(algo, user, item):
      predict = algo.predict(uid=user,iid =item)
      print(predict)
 
+
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
 df_masterdata = master_data.getMasterData()
 df_masterdata = scaleData(df_masterdata)
+input_sample = df_masterdata.head(50)
 
 print(df_masterdata.head())
 print(list(df_masterdata.columns.values))
@@ -43,16 +73,20 @@ data = Dataset.load_from_df(df_masterdata[["userId", "page", "strength"]], reade
 trainset, testset = train_test_split(data, test_size=0.25)
 
 # Train the algorithm on the trainset, and predict ratings for the testset
-algo = SVD(n_factors=100, reg_all=0.05)
-runModel(algo,trainset,testset)
-modelPredict(algo,'f98d1132f60d46883ce49583257104d15ce723b3bbda2147c1e31ac76f0bf069','3325b5a1-979a-4cb3-82b6-63905c9edbe8')
+svd_options = {
+    'n_factors':100,
+    'reg_all' :0.05
+}
 
-sim_options = {
+algo = SVD(n_factors=svd_options["n_factors"], reg_all = svd_options["reg_all"])
+runModel(algo,trainset,testset,'SVD Experimentation',svd_options,"SVD","Experiment with Scikit Surprise SVD model",input_sample)
+
+knn_options = {
     "name": "cosine",
     "user_based": True,  # Compute  similarities between items
 }
 
-algo = KNNBaseline(sim_options=sim_options)
-runModel(algo,trainset,testset)
-modelPredict(algo,'f98d1132f60d46883ce49583257104d15ce723b3bbda2147c1e31ac76f0bf069','3325b5a1-979a-4cb3-82b6-63905c9edbe8')
+algo = KNNBaseline(sim_options=knn_options)
+runModel(algo,trainset,testset,'SVD Experimentation',knn_options,"KNNBaseline","Experiment with Scikit Surprise KNN Baseline model",input_sample)
+
 
