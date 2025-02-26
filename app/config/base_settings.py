1435f00d63b209f0b7ee7e06ff61c6565716e3b9
Pedro Albani - CWI <pedro.martins@cwi.com.br>
2025-02-26 12:46:10 -0300

    Refactor: Replace Dockerfiles and docker-compose with updated configurations; modify database settings and initialization script

--- app/config/base_settings.py
+++ app/config/base_settings.py
@@ -5,18 +5,18 @@ load_dotenv()
 
 class DatabaseConfig:
     def __init__(self):
-        self.mongo_host = os.getenv('MONGO_HOST') or "localhost"
-        self.mongo_port = os.getenv('MONGO_PORT') or 27017
-        self.mongo_username = os.getenv('MONGO_USERNAME')
-        self.mongo_password = os.getenv('MONGO_PASSWORD')
-        self.mongo_database = os.getenv('MONGO_DATABASE')
+        self.mongo_db_tech_5 = os.getenv('mongo_db_tech_5') or "localhost"
+        self.27017 = os.getenv('27017') or 27017
+        self.root = os.getenv('root')
+        self.pass123 = os.getenv('pass123')
+        self.tech_db = os.getenv('tech_db')
         self.mongo_timeout_ms = os.getenv('MONGO_TIMEOUT_MS') or 15000
 
     def get_database_url(self):
-        return f"mongodb://{self.mongo_username}:{self.mongo_password}@{self.mongo_host}:{self.mongo_port}/"
+        return f"mongodb://{self.root}:{self.pass123}@{self.mongo_db_tech_5}:{self.27017}/"
 
     def get_database_name(self):
-        return self.mongo_database
+        return self.tech_db
 
     def get_database_timeout_ms(self):
         return self.mongo_timeout_ms
